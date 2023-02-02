30eeda5e54af2262d2022e0898e35f1e4550c681
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-02 17:28:02 +0300

    '1: Buy peacock feathers' not found in

--- functional_test.py
+++ functional_test.py
@@ -18,34 +18,39 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    def check_for_now_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
+
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(
-            row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-        self.fail('Finish the test')
+        self.check_for_now_in_list_table('1: Buy peacock feathers')
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_now_in_list_table('1: Buy peacock feathers')
+        self.check_for_now_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the test!!!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -5,5 +5,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_return_correct_html(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-        self.assertTemplateUsed(response, 'wro.html')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
