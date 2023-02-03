bf33b5c65dd999c1f22bff627aa37e2ef9344245
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-03 23:54:37 +0300

    LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,9 +4,12 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
 options = Options()
 options.binary_location = r"C:/Program Files/Mozilla Firefox/firefox.exe"
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -20,11 +23,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_now_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_now_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -42,16 +52,14 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_now_in_list_table('1: Buy peacock feathers')
+        self.wait_for_now_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_now_in_list_table('1: Buy peacock feathers')
-        self.check_for_now_in_list_table(
+        self.wait_for_now_in_list_table('1: Buy peacock feathers')
+        self.wait_for_now_in_list_table(
             '2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!!!')
