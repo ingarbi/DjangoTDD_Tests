d541f05b2f047905c7182e72b2cecea66ff815ed
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-05 10:05:06 +0300

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_now_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
--- lists/tests.py
+++ lists/tests.py
@@ -30,6 +30,12 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
 
 class HomePageTest(TestCase):
 
