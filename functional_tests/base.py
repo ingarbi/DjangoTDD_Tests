996f63f1444833dc2eb633079033538486f4f55e
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-08 00:24:00 +0300

    More Advanced Forms. Test func fail bcs needs update on server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -51,6 +51,9 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
