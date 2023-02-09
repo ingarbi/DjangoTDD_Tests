78ba092907101d35a4d6dc758b968103be759c69
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-09 14:22:44 +0300

    Token to url with BUILD_ABSOLUTE_URI

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from accounts import views
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
-    # url(r'^login$', views.login, name='login'),
+    url(r'^login$', views.login, name='login'),
     # url(r'^logout$', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,36 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.contrib import messages
+from accounts.models import Token
+from django.core.urlresolvers import reverse
 
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
-    print(send_mail)
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
         [email]
     )
+    # messages.success(
+    #     request,
+    #     "Check your email, we've sent you a link you can use to log in."
+    # )
+
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+
+def login(request):
+
     return redirect('/')
